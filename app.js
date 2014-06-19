a4fc7ededc092ba9f50a7188f029b1020e4325d7
Romain Quilliot <romain.addweb@gmail.com>
2014-06-19 15:54:23 +0200

    Ajout des modules express

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
-var express = require("express");
-var app = express();
-var config = require("./config.js");
+var express         = require("express");
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+var app             = express();
+var config          = require("./config.js");
 
+
+/**
+ * Initialise les headers
+ *
+ */
+var initHeaders = function (req, res, next)
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE");
+    res.header("Content-type", "application/json");
+
+    next();
+}
+
+
+/**
+ * Modules express
+ */
+app.use(initHeaders);
+app.use(bodyParser());
+app.use(methodOverride());
+
+
+/**
+ * On lance l'app
+ */
 app.listen(3000);
